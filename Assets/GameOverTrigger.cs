faa30bd4672292c320205528b7190941abc43fc3
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2025-04-21 23:52:38 +0530

    Added game reset

--- Assets/GameOverTrigger.cs
+++ Assets/GameOverTrigger.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameOverTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "clone")
+        if (collision.gameObject.tag == "Multiplier" || collision.gameObject.tag =="clone")
         {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
             Debug.Log("Game Over");
         }
     }
